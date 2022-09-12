db81709a15ff163dc6d8740c416858211d3ce8be
moris96 <moriskhoudari@gmail.com>
2022-09-12 17:45:44 -0400

    fixing errors in new app.js

--- app.js
+++ app.js
@@ -26,3 +26,53 @@ const mouse = {
     width: 0.1, 
     height: 0.1,
 }
+
+let canvasPosition = canvas.getBoundingClientRect()
+
+canvas.addEventListener('mousemove', (evt) => {
+    mouse.x = evt.x - canvasPosition.left
+    mouse.y = evt.y - canvasPosition.top
+})
+canvas.addEventListener('mouseleave', () => {
+    mouse.x = undefined
+    mouse.y = undefined
+})
+
+//game board & game board 
+const controlsBar = {
+    width: canvas.width,
+    height: cellSize,
+}
+
+class Cell {
+    constructor(x,y){
+        this.x = x
+        this.y = y
+        this.width = cellSize
+        this.height = cellSize
+    }
+    draw(){
+        if(mouse.x && mouse.y && collision(this, mouse)){
+            ctx.strokeStyle = 'black'
+            ctx.strokeRect(this.x, this.y, this.width, this.height)
+        }
+    }
+}
+const createGrid = () => {
+    for(let y=cellSize; y < canvas.height; y+=cellSize){
+        for(let x=0; x < canvas.width; x+=cellSize){
+            gameGrid.push(new Cell(x,y))
+        }
+    }
+}
+createGrid()
+
+
+
+
+const animate = () => {
+    ctx.clearRect(0,0, canvas.width, canvas.height)
+    ctx.fillStyle = 'blue'
+    ctx.fillRect(0,0, controlsBar.width, controlsBar.height)
+}
+animate();
